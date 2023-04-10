d4002b6eb7a3f7961b1f1797754ff8199d0a0a61
Ippolitov Igor <i.ippolitov@f5.com>
2023-04-10 18:12:43 +0100

    Updated auth spnego module to 1.1.1

--- contrib/src/spnego-http-auth-nginx-module/extra-patch-ngx_http_auth_spnego_module.c
+++ contrib/src/spnego-http-auth-nginx-module/extra-patch-ngx_http_auth_spnego_module.c
@@ -2,27 +2,52 @@ diff --git a/ngx_http_auth_spnego_module.c b/ngx_http_auth_spnego_module.c
 index 97c0b44..831da18 100644
 --- a/ngx_http_auth_spnego_module.c
 +++ b/ngx_http_auth_spnego_module.c
-@@ -339,6 +339,7 @@ ngx_http_auth_spnego_headers_basic_only(
+@@ -505,10 +505,11 @@
+     if (NULL == r->headers_out.www_authenticate) {
+         return NGX_ERROR;
      }
 
      r->headers_out.www_authenticate->hash = 1;
 +    r->headers_out.www_authenticate->next = NULL;
      r->headers_out.www_authenticate->key.len = sizeof("WWW-Authenticate") - 1;
-     r->headers_out.www_authenticate->key.data = (u_char *) "WWW-Authenticate";
+     r->headers_out.www_authenticate->key.data = (u_char *)"WWW-Authenticate";
      r->headers_out.www_authenticate->value.len = value.len;
-@@ -378,6 +379,7 @@ ngx_http_auth_spnego_headers(
+     r->headers_out.www_authenticate->value.data = value.data;
+
+@@ -541,10 +542,11 @@
+     if (NULL == r->headers_out.www_authenticate) {
+         return NGX_ERROR;
      }
 
      r->headers_out.www_authenticate->hash = 1;
 +    r->headers_out.www_authenticate->next = NULL;
      r->headers_out.www_authenticate->key.len = sizeof("WWW-Authenticate") - 1;
-     r->headers_out.www_authenticate->key.data = (u_char *) "WWW-Authenticate";
+     r->headers_out.www_authenticate->key.data = (u_char *)"WWW-Authenticate";
      r->headers_out.www_authenticate->value.len = value.len;
-@@ -399,6 +401,7 @@ ngx_http_auth_spnego_headers(
+     r->headers_out.www_authenticate->value.data = value.data;
+
+@@ -562,10 +564,11 @@
+         if (NULL == r->headers_out.www_authenticate) {
+             return NGX_ERROR;
          }
 
          r->headers_out.www_authenticate->hash = 2;
 +        r->headers_out.www_authenticate->next = NULL;
-         r->headers_out.www_authenticate->key.len = sizeof("WWW-Authenticate") - 1;
-         r->headers_out.www_authenticate->key.data = (u_char *) "WWW-Authenticate";
+         r->headers_out.www_authenticate->key.len =
+             sizeof("WWW-Authenticate") - 1;
+         r->headers_out.www_authenticate->key.data =
+             (u_char *)"WWW-Authenticate";
          r->headers_out.www_authenticate->value.len = value2.len;
+@@ -754,11 +757,11 @@
+
+ static ngx_int_t
+ ngx_http_auth_spnego_store_delegated_creds(ngx_http_request_t *r,
+                                            ngx_str_t *principal_name,
+                                            creds_info delegated_creds) {
+-    krb5_context kcontext;
++    krb5_context kcontext = NULL;
+     krb5_principal principal = NULL;
+     krb5_ccache ccache = NULL;
+     krb5_error_code kerr = 0;
+     char *ccname = NULL;
+     char *escaped = NULL;
